acb20b822261b02f3305eae3403ad52444b990cf
Colin23 <44360448+Colin23@users.noreply.github.com>
2024-05-30 00:13:32 +0200

    Make sure slf4j works as expected

--- src/test/java/com/colinmoerbe/javatodoapp/ApplicationTests.java
+++ src/test/java/com/colinmoerbe/javatodoapp/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.colinmoerbe.javatodoapp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,10 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest;
  * This testing class is currently only for testing the initial setup only.
  */
 @SpringBootTest
-public class ApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
 
+    }
+
+    @Test
+    void exampleTest() {
+        final int a = 2;
+        final int b = 3;
+        final int c = a + b;
+        Assertions.assertEquals(5, c);
     }
 }
