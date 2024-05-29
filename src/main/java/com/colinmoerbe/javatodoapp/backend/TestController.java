d8f29acbde260061bb40cb8d8eea1a8bed9481c2
Colin23 <44360448+Colin23@users.noreply.github.com>
2024-05-29 23:23:04 +0200

    Set up a hierarchie for loading secrets and configuration during runtime

--- src/main/java/com/colinmoerbe/javatodoapp/backend/TestController.java
+++ src/main/java/com/colinmoerbe/javatodoapp/backend/TestController.java
@@ -1,17 +1,26 @@
 package com.colinmoerbe.javatodoapp.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This class is currently only for testing the initial setup only.
+ */
 @RestController
 @RequestMapping("/api")
 class TestController {
+    @Value("${test.password}")
+    private String testPassword;
+
+    @Value("${test.username}")
+    private String testUsername;
 
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return "Testing " + testUsername + " " + testPassword;
     }
 
     @GetMapping("/test/{id}")
