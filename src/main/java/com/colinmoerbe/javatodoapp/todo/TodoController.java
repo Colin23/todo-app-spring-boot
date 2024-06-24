cc5e84de23715c6caed494d726fba7f7c95ab280
Colin23 <44360448+Colin23@users.noreply.github.com>
2024-06-24 02:41:25 +0200

    Add more endpoints to the TodoController

--- src/main/java/com/colinmoerbe/javatodoapp/todo/TodoController.java
+++ src/main/java/com/colinmoerbe/javatodoapp/todo/TodoController.java
@@ -1,15 +1,20 @@
 package com.colinmoerbe.javatodoapp.todo;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -35,27 +40,63 @@ class TodoController {
 
     @GetMapping("/v1/todos")
     List<Todo> getTodos() {
-        LOGGER.info("Successfully called the /v1/todos endpoint");
+        LOGGER.info("Successfully called the GET /api/v1/todos endpoint");
         return todoRepository.findAll();
     }
 
     @GetMapping("/v1/todos/{id}")
     Optional<Todo> getTodo(@PathVariable Integer id) {
-        return null;
+        LOGGER.info("Successfully called the GET /api/v1/todos/" + id + " endpoint");
+        return Optional.ofNullable(todoRepository.findById(id).orElseThrow(TodoNotFoundException::new));
     }
 
     @PutMapping("/v1/todos/{id}")
-    void updateTodo(@PathVariable Integer id) {
-
+    Todo updateTodo(@PathVariable Integer id, @RequestBody Todo todo) {
+        LOGGER.info("Successfully called the PUT /api/v1/todos/" + id + " endpoint");
+        return todoRepository.findById(id)
+            .map(existingTodo -> {
+                existingTodo.setTitle(todo.getTitle());
+                existingTodo.setDescription(todo.getDescription());
+                existingTodo.setDueAt(todo.getDueAt());
+                existingTodo.setCompleted(todo.getCompleted());
+                return todoRepository.save(existingTodo);
+            })
+            .orElseThrow(TodoNotFoundException::new);
     }
 
+    @PatchMapping("/v1/todos/{id}")
+    Todo partiallyUpdateTodo(@PathVariable Integer id, @RequestBody Todo partialTodo) {
+        LOGGER.info("Successfully called the PATCH /api/v1/todos/" + id + " endpoint");
+        return todoRepository.findById(id)
+            .map(existingTodo -> {
+                if (partialTodo.getTitle() != null) {
+                    existingTodo.setTitle(partialTodo.getTitle());
+                }
+                if (partialTodo.getDescription() != null) {
+                    existingTodo.setDescription(partialTodo.getDescription());
+                }
+                if (partialTodo.getDueAt() != null) {
+                    existingTodo.setDueAt(partialTodo.getDueAt());
+                }
+                if (partialTodo.getCompleted() == null) {
+                    existingTodo.setCompleted(partialTodo.getCompleted());
+                }
+                return todoRepository.save(existingTodo);
+            })
+            .orElseThrow(TodoNotFoundException::new);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/v1/todos")
-    public String createTodo() {
-        return "Create method is not yet implemented";
+    Todo createTodo(@RequestBody @Valid Todo todo) {
+        LOGGER.info("Successfully called the POST /api/v1/todos endpoint");
+        return todoRepository.save(todo);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/v1/todos/{id}")
-    public String deleteTodo(@PathVariable Integer id) {
-        return "Delete method is not yet implemented";
+    void deleteTodo(@PathVariable Integer id) {
+        LOGGER.info("Successfully called the DELETE /api/v1/todos/" + id + " endpoint");
+        todoRepository.deleteById(id);
     }
 }
