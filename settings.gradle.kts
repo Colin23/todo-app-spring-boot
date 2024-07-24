60aa08d3f39fbf803461a426f4955760d7ce8175
Colin23 <44360448+Colin23@users.noreply.github.com>
2024-07-24 17:03:29 +0200

    Feat: Improve JDK management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic downloads of JDKs.
+    // This will only trigger if no local Java version matches the necessary version specified in the toolchain.
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "todo-app-spring-boot"
